1b096468af8b95ee8a2ccc76a7de2fd25c6f9f80
Yash <80215154+Yash-9@users.noreply.github.com>
2021-06-01 22:17:23 +0530

    Update p1.java

--- p1.java
+++ p1.java
@@ -2,7 +2,7 @@ public class p1{
       public static void main(strgs[]) {
           system.out.print("Learning Git and GitHub");
           system.out.print("welcome to my project");
-          
+          system.out.print("========");
       }
 
 }
